9994d0c7bb7c2ed90de7bfa2847b37907e7bced4
ikurniawan <business.ikurniawan@gmail.com>
2021-09-23 23:06:33 +0700

    Deploy resource
    mengupload ke repositori

---
+++ assets/js/typedCustom.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    backSpeed: 0,
+    startDelay: 1000,
+    loop: false,
+    loopCount: Infinity,
+  });
+});
